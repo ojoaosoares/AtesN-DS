f444b3375aae08a5bd81e147427c7a84d4b89e3f
Leviathan <ojoaosoaressantos@gmail.com>
2024-05-21 18:02:31 -0300

    Feat: A function that verifies if a packet is IPV4 was created.

--- src/dns.c
+++ src/dns.c
@@ -15,6 +15,35 @@
 
 #define DEBUG
 
+static __always_inline int isIPV4(void *data, __u64 *offset, void *data_end)
+{
+
+    struct ethhdr *eth = data; // Cabeçalho da camada ethrenet
+
+    *offset = sizeof(struct ethhdr);
+
+    if (data + *offset > data_end)
+    {
+        #ifdef DEBUG
+            bpf_printk("[DROP] No ethernet frame");
+        #endif
+
+        return 0;
+    }
+
+    __u16 ip_type; // Tipo de ip, esta contido na camada ethrenet
+    ip_type = eth->h_proto;
+
+    if(ip_type != bpf_htons(IPV4))
+    {
+        #ifdef DEBUG
+            bpf_printk("[DROP] Ethernet type isn't IPV4, %d != %d", ip_type, bpf_htons(IPV4));
+        #endif
+        return 0;
+    }
+
+    return 1;
+}
 
 SEC("dns_filter")
 int dns(struct xdp_md *ctx) {
