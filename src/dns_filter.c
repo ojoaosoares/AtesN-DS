f6981f192b1acb0afd86b867b3febdbc5232a3b0
Soares <joaosoares@dcc.ufmg.br>
2026-05-15 15:04:54 -0300

    feat: dns hw cache

--- src/dns_filter.c
+++ src/dns_filter.c
@@ -22,26 +22,17 @@ struct {
 struct {
     __uint(type, BPF_MAP_TYPE_ARRAY);
     __uint(max_entries, 1);
-    __uint(key_size, 4);
-    __uint(value_size, 4);
+    __uint(key_size, sizeof(__u32));
+    __uint(value_size, sizeof( __u32));
     __uint(map_flags, 0);
-} time_cache SEC(".maps");
-
-struct {
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, 10);
-    __uint(key_size, sizeof(struct rec_query_key));
-    __uint(value_size, sizeof(struct query_owner));
-
-} recursive_queries SEC(".maps");
+} time_map SEC(".maps");
 
 #define EDNS0_OPT_SIZE      11
 #define ETH_HDR_SIZE        14
 #define IP_HDR_SIZE         20
 #define UDP_HDR_SIZE        8
 #define DNS_HDR_SIZE        12
-#define MAX_DNS_NAME_LENGTH 56
-
+#define MAX_DNS_NAME_LENGTH 24
 #define MAX_DNS_QUERY_SIZE  (DNS_HDR_SIZE + MAX_DNS_NAME_LENGTH + 1 + 2 + 2 + EDNS0_OPT_SIZE)
 #define MAX_PACKET_SIZE     (ETH_HDR_SIZE + IP_HDR_SIZE + UDP_HDR_SIZE + MAX_DNS_QUERY_SIZE)
 
@@ -51,11 +42,6 @@ int dns_filter(struct xdp_md *ctx) {
     void *data_end = (void*) (long) ctx->data_end;
     void *data = (void*) (long) ctx->data;
 
-     __u64 pkt_size = data_end - data;
-
-    if (pkt_size > MAX_PACKET_SIZE)
-        return XDP_PASS;
-
     __u64 offset_h = 0;
 
     switch (filter_dns(data, &offset_h, data_end))
@@ -63,10 +49,9 @@ int dns_filter(struct xdp_md *ctx) {
         case DROP:
             return XDP_DROP;
         case PASS:
-        case FROM_DNS_PORT:
             return XDP_PASS;
         default:
-        break;
+            break;
     }
 
     struct dns_query dnsquery = {0};
@@ -83,7 +68,7 @@ int dns_filter(struct xdp_md *ctx) {
         case RESPONSE_RETURN:
             break;
         default:
-            return XDP_DROP;
+            return XDP_PASS;
             break;
     }
     
@@ -102,99 +87,81 @@ int dns_filter(struct xdp_md *ctx) {
             break;
     }
 
+    __u32 *now = 0;
+    __u32 key = 0;
+
+    now = bpf_map_lookup_elem(&time_map, &key);
+
+    if (!now)
+        return XDP_PASS;
+
     if (RESPONSE_RETURN == query_response)
-    {
-        struct rec_query_key curr = {0};
-        __builtin_memset(&curr, 0, sizeof(curr));
+    {   
+        offset_h = ETH_HDR_SIZE + IP_HDR_SIZE + UDP_HDR_SIZE + DNS_HDR_SIZE + domain_size + 5;
 
-        curr.id.id            = dnsquery.id.id;
-        curr.id.port          = get_dest_port(data);
-        curr.query.domain_size = domain_size;
-        struct query_owner *powner = NULL;
+        if (data + offset_h > data_end)
+            return XDP_DROP;
+        
+        struct a_record_hw cache_record = {0};
+        cache_record.ip = 0;
+        cache_record.timestamp = 0;
 
-        powner = bpf_map_lookup_elem(&recursive_queries, (struct rec_query_key *) &curr);
+        __u32 now_value = *now;
+        
+        if (get_dns_answer_hw(data, &offset_h, data_end, &cache_record, now_value) == DROP)
+            return XDP_DROP;
 
-        if (powner)
+        if (cache_record.timestamp)
         {
-            offset_h = ETH_HDR_SIZE + IP_HDR_SIZE + UDP_HDR_SIZE + DNS_HDR_SIZE + domain_size + 5;
-
-            if (data + offset_h > data_end)
-                return XDP_DROP;
-            
-            struct a_record_hw cache_record;
-            cache_record.ip = 0;
-            cache_record.timestamp = 0;
-
-            __u32 key = 0;
-            __u32 *now;
-
-            now = bpf_map_lookup_elem(&time_cache, &key);
-
-            if (!now)
-                return XDP_DROP;
-                
-            if (get_dns_answer_hw(data, &offset_h, data_end, &cache_record, *now) == DROP)
-                return XDP_DROP;
-
-            if (cache_record.timestamp)
-            {
-                bpf_map_update_elem(&level_one_cache, domain_hw.name, &cache_record, BPF_ANY);
-            }
-
+            bpf_map_update_elem(&level_one_cache, domain_hw.name, &cache_record, BPF_ANY);
         }
+
+        return XDP_PASS;
     }
 
-    volatile struct a_record_hw *arecord;
+    struct a_record_hw *arecord;
     arecord = bpf_map_lookup_elem(&level_one_cache, domain_hw.name);
 
     if (arecord)
-    {      
-        __u32 key = 0;
-        __u32 *now;
-
-        now = bpf_map_lookup_elem(&time_cache, &key);
-
-        __u32 diff = 0;
-        
-        if (now)
-            diff = get_ttl_hw(arecord->timestamp, *now);
-
-        if (diff >  MINIMUM_TTL)
+    { 
+	    __u32 now_value = *now;
+        if (arecord->timestamp < now_value)
         {
-            __s16 newsize = (data + offset_h - data_end);
-
-            __u8 status = RCODE_NXDOMAIN;
-
-            if (arecord->ip != 0)
-            {
-                newsize += sizeof(struct dns_response);
-                status = RCODE_NOERROR;
-            }
-
-            if (bpf_xdp_adjust_tail(ctx, (int) newsize) < 0)
-            {    
-                return XDP_DROP;
-            }
-
-            data = (void*) (long) ctx->data;
-            data_end = (void*) (long) ctx->data_end;
-
-            offset_h = 0;
-
-            if (format_network_access_layer_hw(data, &offset_h, data_end) == DROP)
-                 return XDP_DROP;
-        
-            if (swap_internet_layer_hw(data, &offset_h, data_end) == DROP)
-                 return XDP_DROP;
-
-            if (swap_transport_layer(data, &offset_h, data_end) == DROP)
-                 return XDP_DROP;
-
-            if (create_dns_answer(data, &offset_h, data_end, arecord->ip, diff, status, domain_size) == DROP)
-                 return XDP_DROP;
-
-            return XDP_TX;
+            bpf_map_delete_elem(&level_one_cache, domain_hw.name);
+            return XDP_PASS;
         }
+
+        __s16 newsize = (data + offset_h - data_end);
+
+        __u8 status = RCODE_NXDOMAIN;
+
+        if (arecord->ip != 0)
+        {
+            newsize += sizeof(struct dns_response);
+            status = RCODE_NOERROR;
+        }
+
+        if (bpf_xdp_adjust_tail(ctx, (int) newsize) < 0)
+            return XDP_PASS;
+
+        data = (void*) (long) ctx->data;
+        data_end = (void*) (long) ctx->data_end;
+
+        offset_h = 0;
+
+        if (format_network_access_layer_hw(data, &offset_h, data_end) == DROP)
+            return XDP_DROP;
+
+        if (swap_internet_layer_hw(data, &offset_h, data_end) == DROP)
+            return XDP_DROP;
+
+        if (swap_transport_layer(data, &offset_h, data_end) == DROP)
+            return XDP_DROP;
+
+        if (create_dns_answer(data, &offset_h, data_end, arecord->ip, arecord->timestamp, status, domain_size) == DROP)
+            return XDP_DROP;
+
+        return XDP_TX;
     }
 
     return XDP_PASS;
